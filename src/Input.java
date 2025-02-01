c864818d1ac64f78895eabed2ab483eed0e60a2a
Alondrag360 <alondrag4265@gmail.com>
2025-02-01 05:31:22 +0000

    Input

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Input{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int age;
+        double weight;
+        boolean answer;
+        System.out.println("Enter your name: ");
+        name = scanner.next();
+        name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+        System.out.print("Enter your weight: ");
+        weight = scanner.nextDouble();
+        System.out.print("Do you smoke? (YES/NO): ");
+        answer = scanner.nextBoolean();
+        System.out.println("The patient's information is as follows: " + name + age + weight + answer);
+    }
+}
